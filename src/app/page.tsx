462100e6f39aa6098161ef6f3e5a19e1793ea996
Srinivasa Reddy Akumalla <113478192+akumalla-s@users.noreply.github.com>
2024-03-18 12:20:33 -0400

    Initial Commit
    The component contains last 24's miner pool distribution pie chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import styles from "./page.module.css";
 import {graphqlClient} from "@/graphql/client";
 import {BitcoinDocument} from "@/graphql/__generated__/graphql";
+import DistributionChart from "./components/minerdistributionpool/DistributionChart";
 
 async function getBitcoin() {
     return await graphqlClient.request(BitcoinDocument,
@@ -12,15 +13,18 @@ export default async function Home() {
     const bitcoin = await getBitcoin();
 
     return (
-    <main className={styles.main}>
-        <div style={{textAlign: "center"}}>
-          <div>Northeastern Bitcoin Explorer</div>
-          <div>
-              {'# blocks: ' + bitcoin.bitquery.bitcoin?.blocks?.map(b => {
-                  return b.count
-              })}
+      <main className={styles.main}>
+          <div style={{textAlign: "center"}}>
+            <div>Northeastern Bitcoin Explorer</div>
+            <div>
+                {'# blocks: ' + bitcoin.bitquery.bitcoin?.blocks?.map(b => {
+                    return b.count
+                })}
+            </div>
           </div>
-        </div>
-    </main>
+          <div style={{ height: '300px', width: '800px' }}>
+            <DistributionChart/>
+          </div>
+      </main>
   );
 }
