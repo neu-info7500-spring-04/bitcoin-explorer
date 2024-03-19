282e84ec4796f83f2e5d3904b6df8e8bcd9aee11
Srinivasa Reddy Akumalla <113478192+akumalla-s@users.noreply.github.com>
2024-03-19 12:00:37 -0400

    resolve conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,43 @@
+import { BitcoinDocument, BitcoinQuery } from "@/graphql/__generated__/graphql";
+import { graphqlClient } from "@/graphql/client";
 import styles from "./page.module.css";
-import {graphqlClient} from "@/graphql/client";
-import {BitcoinDocument} from "@/graphql/__generated__/graphql";
-import DistributionChart from "./components/minerdistributionpool/DistributionChart";
+import LastBlock from "./LastBlock";
+import RichListChart from "./RichListChart";
 
-async function getBitcoin() {
-    return await graphqlClient.request(BitcoinDocument,
-        {}
-    );
+import React from 'react';
+import { components } from '../components';
+
+
+async function getBitcoin(): Promise<BitcoinQuery> {
+    return await graphqlClient.request(BitcoinDocument, {});
 }
 
 export default async function Home() {
     const bitcoin = await getBitcoin();
+    const lastBlock = bitcoin.bitquery.bitcoin?.blocks?.[0];
+
+    const formatBlockHeight = (height: number | undefined) => {
+        if (height === undefined) return "";
+        const heightString = height.toString();
+        return `${heightString.slice(0, 3)} ${heightString.slice(3)}`;
+    };
 
     return (
-      <main className={styles.main}>
-          <div style={{textAlign: "center"}}>
-            <div>Northeastern Bitcoin Explorer</div>
-            <div>
-                {'# blocks: ' + bitcoin.bitquery.bitcoin?.blocks?.map(b => {
-                    return b.count
-                })}
+        <main className={styles.container}>
+            <div className={styles.block}>
+                <div>Northeastern Bitcoin Explorer</div>
+                <LastBlock bitcoin={bitcoin} />
+                <div className={styles.blockTitle}>Rich chart of Bitcoin addresses</div>
+                <div>
+                    <RichListChart />
+                </div>
+                <div>
+                    <components.POWAndEmission />
+                </div>
             </div>
-          </div>
-          <div style={{ height: '300px', width: '800px' }}>
-            <DistributionChart/>
-          </div>
-      </main>
-  );
+            <div style={{ height: '300px', width: '800px' }}>
+              <DistributionChart/>
+            </div>
+        </main>
+    );
 }
