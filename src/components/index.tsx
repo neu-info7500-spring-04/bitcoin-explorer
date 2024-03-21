c9fd89da1ad21f1efb8d72f65b6cab8d554bfaef
Zhengyu Chen <chen.zhengy@northeastern.edu>
2024-03-20 20:07:52 -0400

    feat: add ispRanking data & graph

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,11 +2,13 @@ import POWAndEmission from "./POWAndEmission";
 import TransactionFeeData from "./transactionFee/transactionFeeData";
 import CountryNodeStats from "./CountryNodeStats";
 import BTCMarketData from "./btc-market-data";
+import RankingData from "./ispRanking/Ranking";
 
 const components = {
     POWAndEmission,
     CountryNodeStats,
     TransactionFeeData,
-    BTCMarketData
+    BTCMarketData,
+    RankingData
 };
 export { components };
