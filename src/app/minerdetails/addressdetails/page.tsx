97368a7f2cfb5be9b3a0e851f4145e1b055ebc1a
Rakshith-Rajkumar-NEU <144841914+Rakshith-Rajkumar@users.noreply.github.com>
2024-03-24 18:40:00 -0400

    bug fixes

--- src/app/minerdetails/addressdetails/page.tsx
+++ src/app/minerdetails/addressdetails/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { useState, useEffect } from 'react';
-import { useSearchParams } from 'next/navigation';
+import { useState, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
 import { graphqlClientWithConstantBase } from "@/graphql/client";
 import { AddressDetailsDocument } from "@/graphql/__generated__/graphql";
 import styles from "./AddressDetails.module.css";
@@ -12,29 +12,31 @@ const AddressDetails = () => {
 
   const fetchData = async (address, from, till) => {
     try {
-      const data = await graphqlClientWithConstantBase.request(AddressDetailsDocument, {
-        since:from,
-        till:till,
-        inputAddress:address,
-        outputAddress:address
-      });
+      const data = await graphqlClientWithConstantBase.request(
+        AddressDetailsDocument,
+        {
+          since: from,
+          till: till,
+          inputAddress: address,
+          outputAddress: address,
+        }
+      );
       setDetails(data);
       console.log(data); // Update state with fetched data
     } catch (error) {
-      console.error('Error fetching address details:', error);
+      console.error("Error fetching address details:", error);
     }
   };
 
   useEffect(() => {
     // Extract query parameters and fetch data when searchParams are available
-    const address = searchParams.get('address');
-    const from = searchParams.get('from');
-    const till = searchParams.get('till');
+    const address = searchParams.get("address");
+    const from = searchParams.get("from");
+    const till = searchParams.get("till");
 
     console.log(address);
     console.log(from);
     console.log(till);
-
 
     if (address && from && till) {
       fetchData(address, from, till);
@@ -50,7 +52,10 @@ const AddressDetails = () => {
 
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>Address Details for <span className={styles.highlight}>{searchParams.get('address')}</span></h2>
+      <h2 className={styles.title}>
+        Address Details for{" "}
+        <span className={styles.highlight}>{searchParams.get("address")}</span>
+      </h2>
       <table className={styles.table}>
         <thead>
           <tr>
@@ -59,13 +64,38 @@ const AddressDetails = () => {
           </tr>
         </thead>
         <tbody>
-          <tr><td>Inputs Count</td><td>{details?.bitquery.bitcoin.inputs[0]?.count || 'N/A'}</td></tr>
-          <tr><td>Outputs Count</td><td>{details?.bitquery.bitcoin.outputs[0]?.count || 'N/A'}</td></tr>
-          <tr><td>Earliest Transaction Date</td><td>{details?.bitquery.bitcoin.inputs[0]?.minimum || 'N/A'}</td></tr>
-          <tr><td>Latest Transaction Date</td><td>{details?.bitquery.bitcoin.outputs[0]?.maximum || 'N/A'}</td></tr>
-          <tr><td>Total Received (BTC)</td><td>{details?.bitquery.bitcoin.outputs[0]?.value.toFixed(8) || 'N/A'}</td></tr>
-          <tr><td>Total Spent (BTC)</td><td>{details?.bitquery.bitcoin.inputs[0]?.value.toFixed(8) || 'N/A'}</td></tr>
-          <tr><td>Balance (BTC)</td><td>{balance.toFixed(8)}</td></tr>
+          <tr>
+            <td>Inputs Count</td>
+            <td>{details?.bitquery.bitcoin.inputs[0]?.count || "N/A"}</td>
+          </tr>
+          <tr>
+            <td>Outputs Count</td>
+            <td>{details?.bitquery.bitcoin.outputs[0]?.count || "N/A"}</td>
+          </tr>
+          <tr>
+            <td>Earliest Transaction Date</td>
+            <td>{details?.bitquery.bitcoin.inputs[0]?.minimum || "N/A"}</td>
+          </tr>
+          <tr>
+            <td>Latest Transaction Date</td>
+            <td>{details?.bitquery.bitcoin.outputs[0]?.maximum || "N/A"}</td>
+          </tr>
+          <tr>
+            <td>Total Received (BTC)</td>
+            <td>
+              {details?.bitquery.bitcoin.outputs[0]?.value.toFixed(8) || "N/A"}
+            </td>
+          </tr>
+          <tr>
+            <td>Total Spent (BTC)</td>
+            <td>
+              {details?.bitquery.bitcoin.inputs[0]?.value.toFixed(8) || "N/A"}
+            </td>
+          </tr>
+          <tr>
+            <td>Balance (BTC)</td>
+            <td>{balance.toFixed(8)}</td>
+          </tr>
         </tbody>
       </table>
     </div>
--- src/app/minerdetails/page.tsx
+++ src/app/minerdetails/page.tsx
@@ -2,28 +2,35 @@
 import styles from "./MinerDetails.module.css";
 import { graphqlClientWithConstantBase } from "@/graphql/client";
 import { MinerDataDocument } from "@/graphql/__generated__/graphql";
-import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
 
-
-function getBitcoinData(limit: number, offset: number, from: string, till: string) {
-  return graphqlClientWithConstantBase.request(MinerDataDocument, {
-    limit: limit,
-    offset: offset,
-    from: from,
-    till: till
-  })
-  .then(response => response.bitquery.bitcoin.outputs)
-  .catch(error => {
-    console.error('Error fetching Bitcoin data:', error);
-    throw new Error('Error fetching Bitcoin data');
-  });
+function getBitcoinData(
+  limit: number,
+  offset: number,
+  from: string,
+  till: string
+) {
+  return graphqlClientWithConstantBase
+    .request(MinerDataDocument, {
+      limit: limit,
+      offset: offset,
+      from: from,
+      till: till,
+    })
+    .then((response) => response.bitquery.bitcoin.outputs)
+    .catch((error) => {
+      console.error("Error fetching Bitcoin data:", error);
+      throw new Error("Error fetching Bitcoin data");
+    });
 }
 
 export default function Home() {
   const limit = 10;
   const offset = 0;
-  const [fromDate, setFromDate] = useState(new Date(new Date().setDate(new Date().getDate() - 7)));
+  const [fromDate, setFromDate] = useState(
+    new Date(new Date().setDate(new Date().getDate() - 7))
+  );
   const [tillDate, setTillDate] = useState(new Date());
   const [bitcoinData, setBitcoinData] = useState([]);
 
@@ -33,38 +40,44 @@ export default function Home() {
 
   const fetchBitcoinData = () => {
     getBitcoinData(limit, offset, fromDate, tillDate)
-      .then(data => {
+      .then((data) => {
         setBitcoinData(data);
       })
-      .catch(error => {
-        console.error('Error fetching data:', error);
+      .catch((error) => {
+        console.error("Error fetching data:", error);
       });
   };
 
   useEffect(() => {
-    fetchBitcoinData(); 
+    fetchBitcoinData();
   }, []);
 
   return (
     <main className={styles.main}>
       <div className={styles.tableContainer}>
-      <h1 style={{ textAlign: 'center', color:'black'}}>Miner and Address statistics</h1>
-
+        <h1 style={{ textAlign: "center", color: "black" }}>
+          Miner and Address statistics
+        </h1>
 
         <div>
-          <label style={{ color: 'black' }} htmlFor="fromDate">From: </label>
-          <input 
-            type="date" 
-            id="fromDate" 
-            value={fromDate.toISOString().split('T')[0]}
+          <label style={{ color: "black" }} htmlFor="fromDate">
+            From:{" "}
+          </label>
+          <input
+            type="date"
+            id="fromDate"
+            value={fromDate.toISOString().split("T")[0]}
             onChange={(e) => setFromDate(new Date(e.target.value))}
           />
 
-          <label style={{ color: 'black' }} htmlFor="tillDate"> To: </label>
-          <input 
-            type="date" 
-            id="tillDate" 
-            value={tillDate.toISOString().split('T')[0]}
+          <label style={{ color: "black" }} htmlFor="tillDate">
+            {" "}
+            To:{" "}
+          </label>
+          <input
+            type="date"
+            id="tillDate"
+            value={tillDate.toISOString().split("T")[0]}
             onChange={(e) => setTillDate(new Date(e.target.value))}
           />
 
@@ -74,28 +87,32 @@ export default function Home() {
         <table className={styles.table}>
           <thead>
             <tr>
-            <th style={{ color: 'black' }}>Address</th>
-              <th style={{ color: 'black' }}>Block Count</th>
-              <th style={{ color: 'black' }}>Minimum Date</th>
-              <th style={{ color: 'black' }}>Maximum Date</th>
-              <th style={{ color: 'black' }}>Block Reward (BTC)</th>
+              <th style={{ color: "black" }}>Address</th>
+              <th style={{ color: "black" }}>Block Count</th>
+              <th style={{ color: "black" }}>Minimum Date</th>
+              <th style={{ color: "black" }}>Maximum Date</th>
+              <th style={{ color: "black" }}>Block Reward (BTC)</th>
             </tr>
           </thead>
           <tbody>
             {bitcoinData.map((output, index) => (
               <tr key={index}>
                 <td>
-                <Link 
-        href={`/minerdetails/addressdetails?address=${output.outputAddress.address}&from=${fromDate.toISOString().split('T')[0]}&till=${tillDate.toISOString().split('T')[0]}`}
-        className={styles.addressLink} // Apply styles directly to Link if necessary
-      >
-        {output.outputAddress.address}
-      </Link>
+                  <Link
+                    href={`/minerdetails/addressdetails?address=${
+                      output.outputAddress.address
+                    }&from=${fromDate.toISOString().split("T")[0]}&till=${
+                      tillDate.toISOString().split("T")[0]
+                    }`}
+                    className={styles.addressLink} // Apply styles directly to Link if necessary
+                  >
+                    {output.outputAddress.address}
+                  </Link>
                 </td>
-                <td style={{ color: 'black' }}>{output.count}</td>
-                <td style={{ color: 'black' }}>{output.minimum}</td>
-                <td style={{ color: 'black' }}>{output.maximum}</td>
-                <td style={{ color: 'black' }}>{output.value.toFixed(8)}</td>
+                <td style={{ color: "black" }}>{output.count}</td>
+                <td style={{ color: "black" }}>{output.minimum}</td>
+                <td style={{ color: "black" }}>{output.maximum}</td>
+                <td style={{ color: "black" }}>{output.value.toFixed(8)}</td>
               </tr>
             ))}
           </tbody>
