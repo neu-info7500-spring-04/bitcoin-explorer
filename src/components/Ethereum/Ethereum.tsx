378d513050af9c295663a2ec438a1c2ac784c675
RanjithKumar839 <114045489+RanjithKumar839@users.noreply.github.com>
2024-03-25 16:20:09 -0400

    Update Ethereum.tsx

--- src/components/Ethereum/Ethereum.tsx
+++ src/components/Ethereum/Ethereum.tsx
@@ -1,6 +1,8 @@
 import { GraphQLClient, gql } from "graphql-request";
 import React from "react";
 
+import '../Ethereum/Ethereum.css';
+
 export default async function Ethereum() {
   const endpoint =  process.env.GRAPHQL_API_GATEWAY_URL as string;
   const graphClient = new GraphQLClient(endpoint);
