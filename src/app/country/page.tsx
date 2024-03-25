8ab38b2091c628eb1a685776cb0bf63863d59846
Rakshith Rajkumar NEU <rajkumar.rak@northeastern.edu>
2024-03-24 23:10:12 -0400

    bug fixes (#95)
    * bug fixes
    
    * Squashed commit of the following:
    
    commit 21ad6c4fedd28deb8c4e27192c354c7f985a325a
    Author: Dheeraj Nandyala <113545138+dheeru99@users.noreply.github.com>
    Date:   Sun Mar 24 22:28:31 2024 -0400
    
        intial changes (#91)

--- src/app/country/page.tsx
+++ src/app/country/page.tsx
@@ -3,15 +3,8 @@
 import { CountryProvider } from "./CountryProvider";
 import { ActiveNodesSummary } from "./ActiveNodesSummary";
 import CountryList from "./CountryList";
-import { graphqlClient } from "@/graphql/client";
-import { ActiveNodesDocument } from "@/graphql/__generated__/graphql";
-
-export async function getSnapshot() {
-  return await graphqlClient.request(ActiveNodesDocument, {});
-}
 
 export default async function page() {
-  const activeNodes = await getSnapshot();
   return (
     <main>
       <CountryProvider>
