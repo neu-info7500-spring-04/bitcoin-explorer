2e731ab32189030c7c90ca2c68941b7fa15fc1d8
Jainil Malaviya <malaviya.j@northeastern.edu>
2024-03-26 07:13:31 +0530

    Squashed commit of the following:
    commit ac01d2abf9152f8378ca0136289ee5c6af2b6127
    Author: ashisland11 <115855366+ashisland11@users.noreply.github.com>
    Date:   Mon Mar 25 17:13:01 2024 -0700
    
        Yazhen Han Latest Bitcoin Blocks (#143)
    
        * Yazhen Han Latest Bitcoin Blocks
    
        * ''
    
        ---------
    
        Co-authored-by: yazhen <yazhenhan1998@gmail.com>
    
    commit bd27ffe080747f0d1393e492675a81bc4ab9e5d8
    Author: RanjithKumar839 <114045489+RanjithKumar839@users.noreply.github.com>
    Date:   Mon Mar 25 19:03:35 2024 -0400
    
        Update Ethereum.tsx (#153)
    
    commit 630b720a408605bc9412cc070b9cddea73e57a9f
    Author: RanjithKumar839 <114045489+RanjithKumar839@users.noreply.github.com>
    Date:   Mon Mar 25 18:50:35 2024 -0400
    
        Ranjith ethereum (#150)
    
        * Update Ethereum.tsx
    
        * Update Ethereum.tsx
    
        * Update Ethereum.tsx
    
        * Update Ethereum.tsx
    
        * Update Ethereum.tsx
    
    commit 283976b832aa8bd12dcfecb0d491f40db08ef5ac
    Author: Sidharth Reddy Pandiri <113542771+SidharthReddy11265@users.noreply.github.com>
    Date:   Mon Mar 25 17:56:25 2024 -0400
    
        Update BitcoinTransaction.tsx (#142)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import DistributionChart from "./components/minerdistributionpool/DistributionCh
 import MinerDetails from "./minerdetails/page";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CryptoMarketData from "../components/CryptoMarketData";
+import BitcoinBlocks from '@/pages/LatestBlocks/BitcoinBlocks'
 import Link from "next/link";
 import BlockTransactions from "./BlockTransactions";
 
@@ -47,7 +48,7 @@ async function getMempoolCountryNodes(): Promise<MempoolQuery> {
 export default async function Home() {
   const bitcoin = await getBitcoin();
 
-  const lastBlock = bitcoin.bitquery.bitcoin?.blocks?.[0];
+  const lastestBlock = bitcoin.bitquery.bitcoin?.blocks?.[0];
 
   const formatBlockHeight = (height: number | undefined) => {
     if (height === undefined) return "";
@@ -145,8 +146,16 @@ export default async function Home() {
         <MempoolRecent />
       </div>
 
-      <div className={styles.containerRow}>
-        <components.DailyBlockCountData />
+        <div className={styles.containerRow}>
+          <components.DailyBlockCountData />
+        </div>
+
+      <div>
+        <BitcoinBlocks/>
+      </div>
+
+      <div>
+        <CryptoMarketData/>
       </div>
 
       <div>
