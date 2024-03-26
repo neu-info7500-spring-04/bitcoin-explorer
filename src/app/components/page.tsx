749ba7d2f578d568c2987ec3b81803f636920816
Jainil Malaviya <malaviya.j@northeastern.edu>
2024-03-26 08:31:56 +0530

    Added Block Details

---
+++ src/app/components/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+
+const TransactionDetailsGrid = ({ data }: { data: any }) => {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateRows: "repeat(4, auto)",
+        gridGap: "10px",
+      }}
+    >
+      {Object.entries(data).map(([key, value]) => (
+        <div key={key} style={gridRowStyle}>
+          <div>{key}</div>
+          <div>{typeof value == "object" ? JSON.stringify(value) : value}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const gridRowStyle = {
+  display: "grid",
+  gridTemplateColumns: "1fr 2fr",
+  border: "1px solid #000",
+  padding: "10px",
+};
+
+const SingleTransaction = () => {
+  const searchParams = useSearchParams();
+
+  const transaction = searchParams.get("transaction");
+  const transactionData = transaction ? JSON.parse(transaction) : null;
+
+  return (
+    <div>
+      <h2>Single Transaction Details</h2>
+      {transactionData && (
+        <div>
+          <p>Hash: {transactionData.hash}</p>
+          <p>
+            Amount Transacted: {transactionData.inputValue.toFixed(8)} (
+            {transactionData.input_value_usd.toFixed(2)} USD)
+          </p>
+        </div>
+      )}
+      <div>
+        {transactionData && <TransactionDetailsGrid data={transactionData} />}
+      </div>
+    </div>
+  );
+};
+const gridItemStyle = {
+  border: "1px solid #000",
+  padding: "20px",
+};
+export default SingleTransaction;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ import React from "react";
 import { components } from "../components";
 import DistributionChart from "./components/minerdistributionpool/DistributionChart";
 import MinerDetails from "./minerdetails/page";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import CryptoMarketData from "../components/CryptoMarketData";
 import Link from "next/link";
 
@@ -24,149 +24,155 @@ import BitcoinInfo from "../components/BitcoinInfo";
 import Fees from "../components/Fees";
 import "../components/MainContent.css";
 import BarGraph from "../components/BarGraph";
-import MempoolRecent from "@/components/mempoolRecentTransactions/MempoolRecent";
+// import MempoolRecent from "@/components/mempoolRecentTransactions/MempoolRecent";
 import LiquidTransaction from "@/components/LiquidTransaction/LiquidTransaction";
 import Assets from "@/components/Assets/Assets";
 import BitcoinTransaction from "@/components/BitcoinTransaction/BitcoinTransaction";
 import Ethereum from "@/components/Ethereum/Ethereum";
 
-async function getBitcoin(): Promise<BitcoinQuery> {
-  return await graphqlClient.request(BitcoinDocument, {});
-}
+// async function getBitcoin(): Promise<BitcoinQuery> {
+//   return await graphqlClient.request(BitcoinDocument, {});
+// }
 
-async function getMempoolCountryNodes(): Promise<MempoolQuery> {
-  try {
-    return await graphqlClient.request(MempoolDocument, {});
-  } catch (error) {
-    console.log("Failed to fetch mempool data", error);
-    throw error;
-  }
-}
+// async function getMempoolCountryNodes(): Promise<MempoolQuery> {
+//   try {
+//     return await graphqlClient.request(MempoolDocument, {});
+//   } catch (error) {
+//     console.log("Failed to fetch mempool data", error);
+//     throw error;
+//   }
+// }
 
 export default async function Home() {
-  const bitcoin = await getBitcoin();
+  // const bitcoin = await getBitcoin();
 
-  const lastBlock = bitcoin.bitquery.bitcoin?.blocks?.[0];
+  // const lastBlock = bitcoin.bitquery.bitcoin?.blocks?.[0];
 
-  const formatBlockHeight = (height: number | undefined) => {
-    if (height === undefined) return "";
-    const heightString = height.toString();
-    return `${heightString.slice(0, 3)} ${heightString.slice(3)}`;
-  };
+  // const formatBlockHeight = (height: number | undefined) => {
+  //   if (height === undefined) return "";
+  //   const heightString = height.toString();
+  //   return `${heightString.slice(0, 3)} ${heightString.slice(3)}`;
+  // };
 
-  const mempoolCountryNodes = await getMempoolCountryNodes();
+  // const mempoolCountryNodes = await getMempoolCountryNodes();
 
   return (
-    <main className={styles.container} id="main">
-      <div className={styles.block}>
-        <div>Northeastern Bitcoin Explorer</div>
-        <LatestBlocks />
-        <div className={styles.containerRow}>
-          <div className={styles.containerRow}>
-            <LastBlock bitcoin={bitcoin} />
-          </div>
-          <div className={styles.containerRow}>
-            <components.BTCMarketData />
-          </div>
-        </div>
-        <div className={styles.blockTitle}>Rich chart of Bitcoin addresses</div>
-        {/*<div>
-          <RichListChart />
-        </div>*/}
-        {/* Link to Active Node details route */}
-        <div className="relative border-2 border-white-500 rounded-lg p-2 font-bold hover:border-2 hover:border-blue hover:bg-blue hover:text-blue">
-          <Link
-            className="after:absolute after:inset-0 no-underline "
-            href="/country"
-          >
-            Display Active Node Details
-          </Link>
-        </div>
-        <div>
-          <components.POWAndEmission />
-        </div>
-        <div className={styles.containerRow}>
-          <components.TransactionFeeData />
-        </div>
-        <div className="main-content">
-          <div className="info-content">
-            <BitcoinInfo />
-            <BarGraph />
-            <div className="vertical-separator"></div>
-            <Fees />
-          </div>
-          <hr className="section-separator" />
-          <div className="info-content">
-            <MarketData />
-          </div>
-        </div>
-        <div>
-          <Blocklists />
-        </div>
-      </div>
-      <components.CountryNodeStats mempoolCountryNodes={mempoolCountryNodes} />
-      <div style={{ height: "300px", width: "800px" }}>
-        <DistributionChart />
-      </div>
+    // <main className={styles.container} id="main">
+    //   <div className={styles.block}>
+    //     <div>Northeastern Bitcoin Explorer</div>
+    //     <LatestBlocks />
+    //     <div className={styles.containerRow}>
+    //       <div className={styles.containerRow}>
+    //         <LastBlock bitcoin={bitcoin} />
+    //       </div>
+    //       <div className={styles.containerRow}>
+    //         <components.BTCMarketData />
+    //       </div>
+    //     </div>
+    //     <div className={styles.blockTitle}>Rich chart of Bitcoin addresses</div>
+    //     {/*<div>
+    //       <RichListChart />
+    //     </div>*/}
+    //     {/* Link to Active Node details route */}
+    //     <div className="relative border-2 border-white-500 rounded-lg p-2 font-bold hover:border-2 hover:border-blue hover:bg-blue hover:text-blue">
+    //       <Link
+    //         className="after:absolute after:inset-0 no-underline "
+    //         href="/country"
+    //       >
+    //         Display Active Node Details
+    //       </Link>
+    //     </div>
+    //     <div>
+    //       <components.POWAndEmission />
+    //     </div>
+    //     <div className={styles.containerRow}>
+    //       <components.TransactionFeeData />
+    //     </div>
+    //     <div className="main-content">
+    //       <div className="info-content">
+    //         <BitcoinInfo />
+    //         <BarGraph />
+    //         <div className="vertical-separator"></div>
+    //         <Fees />
+    //       </div>
+    //       <hr className="section-separator" />
+    //       <div className="info-content">
+    //         <MarketData />
+    //       </div>
+    //     </div>
+    //     <div>
+    //       <Blocklists />
+    //     </div>
+    //   </div>
+    //   <components.CountryNodeStats mempoolCountryNodes={mempoolCountryNodes} />
+    //   <div style={{ height: "300px", width: "800px" }}>
+    //     <DistributionChart />
+    //   </div>
 
-      <div style={{ marginTop: "90px", width: "100%" }}>
-        <MinerDetails />
-      </div>
-      <div
-        style={{
-          marginTop: "50px",
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <h2>Enter address to get UTXO distribution statistics</h2>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            width: "100%",
-          }}
-        >
-          <div style={{ marginRight: "20px" }}>
-            <UtxoChart />
-          </div>
-          <div>
-            <UtxoPieChart />
-          </div>
-        </div>
-      </div>
-      <components.RankingData />
+    //   <div style={{ marginTop: "90px", width: "100%" }}>
+    //     <MinerDetails />
+    //   </div>
+    //   <div
+    //     style={{
+    //       marginTop: "50px",
+    //       width: "100%",
+    //       display: "flex",
+    //       flexDirection: "column",
+    //       alignItems: "center",
+    //     }}
+    //   >
+    //     <h2>Enter address to get UTXO distribution statistics</h2>
+    //     <div
+    //       style={{
+    //         display: "flex",
+    //         justifyContent: "center",
+    //         alignItems: "center",
+    //         width: "100%",
+    //       }}
+    //     >
+    //       <div style={{ marginRight: "20px" }}>
+    //         <UtxoChart />
+    //       </div>
+    //       <div>
+    //         <UtxoPieChart />
+    //       </div>
+    //     </div>
+    //   </div>
+    //   <components.RankingData />
 
-      <div style={{ marginTop: "20px", width: "80%", display: "flex" }}>
-        <MempoolRecent />
-      </div>
+    //   <div style={{ marginTop: "20px", width: "80%", display: "flex" }}>
+    //     <MempoolRecent />
+    //   </div>
 
-      <div className={styles.containerRow}>
-        <components.DailyBlockCountData />
-      </div>
+    //   <div className={styles.containerRow}>
+    //     <components.DailyBlockCountData />
+    //   </div>
 
-      <div>
-        <CryptoMarketData />
-      </div>
+    //   <div>
+    //     <CryptoMarketData />
+    //   </div>
 
+    //   <div>
+    //     <h1>Transactions</h1>
+    //     <TransactionDetails />
+    //   </div>
+    //   <div>
+    //   <LiquidTransaction />
+    //   </div>
+    //   <div>
+    //   <Assets />
+    //   </div>
+    //   <div>
+    //     <BitcoinTransaction />
+    //   </div>
+    //   <div>
+    //     <Ethereum />
+    //   </div>
+    // </main>
+    <main>
       <div>
-        <h1>Transactions</h1>
-        <TransactionDetails />
-      </div>
-      <div>
-      <LiquidTransaction />
-      </div>
-      <div>
-      <Assets />
-      </div>
-      <div>
-        <BitcoinTransaction />
-      </div>
-      <div>
-        <Ethereum />
+           <BitcoinTransaction />
+
       </div>
     </main>
   );
