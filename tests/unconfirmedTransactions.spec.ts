8bf4f83ad44468a6defcd88b7eaa8d90e8204d31
shreycodes1 <160746061+shreycodes1@users.noreply.github.com>
2024-03-28 14:14:31 -0400

    Update unconfirmedTransactions.spec.ts

--- tests/unconfirmedTransactions.spec.ts
+++ tests/unconfirmedTransactions.spec.ts
@@ -1,10 +1,1 @@
-import { test, expect } from '@playwright/test';
 
-test('UnconfirmedTransactions component is rendered', async ({ page }) => {
-  // Navigate to the page where your React app is running
-  await page.goto('http://localhost:3000');
-
-  // Locate the element with the class 'transactions-header' and check if it has the expected text
-  // const transactionsHeader = await page.locator('.transactions-header');
-  // await expect(transactionsHeader).toHaveText('Unconfirmed Transactions', { timeout: 10000 }); // Increase timeout to 10 seconds if needed
-});
