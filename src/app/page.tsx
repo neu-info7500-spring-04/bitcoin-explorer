f297651c91ae1fc57bd4d815f9f815ce47618659
Kushal Patel <imkushalpatel@live.com>
2024-03-28 00:48:56 -0400

    Added charts and block with transaction details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,6 +36,7 @@ import Ethereum from "@/components/Ethereum/Ethereum";
 import Statistics from './components/best-fee/Statistics.tsx'
 import LineChart from './components/best-fee/LineChart'
 import Bitcoinassetdata from "./bitcoinassetdata";
+import BlockWithTransactionDetails from "@/pages/BlockWithTransactionDetails/BlockWithDetails";
 
 async function getBitcoin(): Promise<BitcoinQuery> {
   return await graphqlClient.request(BitcoinDocument, {});
---
+++ src/pages/BlockWithTransactionDetails/BlockWithDetails.tsx
@@ -0,0 +1,20 @@
+"use client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import BitcoinChartsPage from "./components/ChartPage";
+import BlockList from "./components/BlockList";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+export default function BlockWithDetails() {
+  return (
+    <Container>
+      <Row>
+        <BlockList />
+      </Row>
+
+      <Row>
+        <BitcoinChartsPage />
+      </Row>
+    </Container>
+  );
+}
