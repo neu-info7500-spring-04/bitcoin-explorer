df13a95c56850d70ed1ccae1cee799c4e76012ab
Junchao Mao <52117946+OHMFOHS@users.noreply.github.com>
2024-04-02 17:34:44 -0700

    add coinmarket (#197)

---
+++ contributors/junchaomao.md
@@ -0,0 +1,6 @@
+Name: Junchao Mao Username: @OHMFOHS
+
+Contributions: 
+*Create CSS to mimic https://blockexplorer.one/markets UI
+*Add CoinMarket component to display real time market performance of Bitcoin and other currencies
+*Use Rest API RapidAPI-Coinranking to fetch coin market information and supply
--- src/types.ts
+++ src/types.ts
@@ -60,3 +60,16 @@ export interface ApiResponse {
     cryptoapis: CryptoapisResponse;
   };
 }
+
+export interface CryptoCurrency {
+  id: number;
+  name: string;
+  iconUrl: string;
+  marketCap: string;
+  price: string;
+  rank: string;
+  uuid: string;
+  circulatingSupply?: string;
+  maxSupply?: string;
+  change: string;
+}
